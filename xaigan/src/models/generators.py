17a5704c44c7c7107885002175c35aa0b74f0317
Yehia Ahmed <yehia.ahmed@ws-211001.esk.fraunhofer.de>
2023-01-16 11:28:20 +0100

    cleaning and refactoring

--- xaigan/src/models/generators.py
+++ xaigan/src/models/generators.py
@@ -1,5 +1,6 @@
 from abc import ABC
 from torch import nn, Tensor
+import torch
 import numpy as np
 
 #============================================ start of ENCODERS =======================================================================================
@@ -52,26 +53,25 @@ class GeneratorNetCIFAR10(nn.Module, ABC):
     def __init__(self,n_features=100):
         super(GeneratorNetCIFAR10, self).__init__()
         self.n_features = n_features
-        #self.n_out = (3, 32, 32)
         self.n_out = (3, 32, 32)
 
         nc, nz, ngf = 3, n_features, 64
 
         self.input_layer = nn.Sequential(
             nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(ngf * 8),#nn.BatchNorm2d(ngf * 8),
+            nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
         )
 
         self.hidden1 = nn.Sequential(
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 4),#nn.BatchNorm2d(ngf * 4),
+            nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
         )
 
         self.hidden2 = nn.Sequential(
             nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 2),#nn.BatchNorm2d(ngf * 2),
+            nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
         )
 
@@ -81,17 +81,10 @@ class GeneratorNetCIFAR10(nn.Module, ABC):
         )
 
     def forward(self, x):
-        #print("\tdense_emb/Gener Input shape is",x.shape)
-        # x = x[:,:, np.newaxis, np.newaxis]
-        #print("\t1 maniplulation to that input, it becomes is",x.shape)
         x = self.input_layer(x)
-        #print("output of Gen input_layer: ", x.size())
         x = self.hidden1(x)
-        #print("Gen hidden1 layer: ", x.size())
         x = self.hidden2(x)
-        #print("Gen hidden2 layer: ", x.size())
         x = self.out(x)
-        #print("Gen out layer: ", x.size())
         return x
 
 
@@ -100,26 +93,25 @@ class GeneratorNet_MSCOCO(nn.Module, ABC):
     def __init__(self,n_features=100):
         super(GeneratorNet_MSCOCO, self).__init__()
         self.n_features = n_features
-        #self.n_out = (3, 32, 32)
         self.n_out = (3, 256, 256)
 
         nc, nz, ngf = 3, n_features, 64
 
         self.input_layer = nn.Sequential(
             nn.ConvTranspose2d(nz, ngf * 8, 32, 1, 0, bias=False),
-            nn.BatchNorm2d(ngf * 8),#nn.BatchNorm2d(ngf * 8),
+            nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
         )
 
         self.hidden1 = nn.Sequential(
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 4),#nn.BatchNorm2d(ngf * 4),
+            nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
         )
 
         self.hidden2 = nn.Sequential(
             nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 2),#nn.BatchNorm2d(ngf * 2),
+            nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
         )
 
@@ -129,24 +121,16 @@ class GeneratorNet_MSCOCO(nn.Module, ABC):
         )
 
     def forward(self, x):
-        #print("\tdense_emb/Gener Input shape is",x.shape)
-        # x = x[:,:, np.newaxis, np.newaxis]
-        #print("\t1 maniplulation to that input, it becomes is",x.shape)
         x = self.input_layer(x)
-        #print("output of Gen input_layer: ", x.size())
         x = self.hidden1(x)
-        #print("Gen hidden1 layer: ", x.size())
         x = self.hidden2(x)
-        #print("Gen hidden2 layer: ", x.size())
         x = self.out(x)
-        #print("Gen out layer: ", x.size())
         return x
 
 class GeneratorNet_2_MSCOCO(nn.Module, ABC):
     def __init__(self,n_features=100):
         super(GeneratorNet_2_MSCOCO, self).__init__()
         self.n_features = n_features
-        #self.n_out = (3, 32, 32)
         size=256
         self.n_out = (3, size, size)
         nc, nz, ngf = 3, n_features, 64
@@ -189,15 +173,11 @@ class GeneratorNet_2_MSCOCO(nn.Module, ABC):
         )
 
     def forward(self, x):
-        #print("\tdense_emb/Gener Input shape is",x.shape)
-        # x = x[:,:, np.newaxis, np.newaxis]
-        #print("\t1 maniplulation to that input, it becomes is",x.shape)
         x = self.input_layer(x)
         x = self.hidden1(x)
         x = self.hidden2(x)
         x = self.hidden3(x)
         x = self.out(x)
-        #print("Gen out layer: ", x.size())
         return x
 
 
@@ -218,7 +198,6 @@ class Generator_Encoder_Net_MSCOCO(nn.Module, ABC):
         
     def forward(self, x):
         x = self.encoder(x)
-        # x = x.reshape(-1,self.generator_feat)  #TODO:investigate 
         x = x[:,:, np.newaxis, np.newaxis] #add new access to pass to generator
         x = self.generator(x)
         return x
@@ -239,7 +218,6 @@ class GeneratorNet_2_EncoderNet_MSCOCO(nn.Module, ABC):
     def forward(self, x):
         x = self.encoder(x)
         x = x[:,:, np.newaxis, np.newaxis] #add new access to pass to generator
-        # x = x.reshape(-1,self.generator_feat)  #TODO:investigate 
         x = self.generator(x)
         return x
 
@@ -260,7 +238,6 @@ class Generator_Encoder_Net_CIFAR10(nn.Module, ABC):
         
     def forward(self, x):
         x = self.encoder(x)
-        # x = x.reshape(-1,self.generator_feat)  #TODO:investigate 
         x = x[:,:, np.newaxis, np.newaxis] #add new access to pass to generator
         x = self.generator(x)
         return x
